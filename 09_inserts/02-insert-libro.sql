1ed67d277e9490429db45865b7489416be4fa380
Jhon Camilo Suaza <jhon04suazasanchez@gmail.com>
2026-03-25 09:21:07 -0500

    HU-04: inserts tabla libro

---
+++ 09_inserts/02-insert-libro.sql
@@ -0,0 +1,6 @@
+--changeset jhon04suazasanchez@gmail.com:09_inserts_libro
+INSERT INTO libro (titulo, autor, isbn, anio_publicacion, cantidad_disponible)
+VALUES 
+('Cien años de soledad', 'Gabriel García Márquez', 'ISBN001', 1967, 5),
+('El principito', 'Antoine de Saint-Exupéry', 'ISBN002', 1943, 10),
+('Don Quijote de la Mancha', 'Miguel de Cervantes', 'ISBN003', 1605, 3);
---
+++ 11_rollbacks/09_inserts/02-insert-libro-rollback.sql
@@ -0,0 +1,2 @@
+--hangeset jhon04suazasanchez@gmail.com:09_inserts_libro_rollback
+DELETE FROM libro WHERE isbn IN ('ISBN001','ISBN002','ISBN003');
